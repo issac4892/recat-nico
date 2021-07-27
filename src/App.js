7c5eb7ff748f292e442a4ff0d070dcdcf6567d7d
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-07-27 23:46:23 +0900

    i sleep

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
+import React from 'react';
+
 const foodILike = [
   {
     key: 1,
-    name: "Kimchi",
+    name: 'Kimchi',
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
   },
   {
     key: 2,
-    name: "Samgyeopsal",
+    name: 'Samgyeopsal',
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
   },
   {
     key: 3,
-    name: "Bibimbap",
+    name: 'Bibimbap',
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
   },
   {
     key: 4,
-    name: "Doncasu",
+    name: 'Doncasu',
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
   },
   {
     key: 5,
-    name: "Kimbap",
+    name: 'Kimbap',
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-]
+      'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
+  },
+];
 
-function Food({ name, picture }) {
-  const altname = "Supposed to be a " + name + " picture"
+function Food({name, picture}) {
+  const altname = name + ' picture';
   return (
     <div>
       <h2>I like {name}</h2>
       <img src={picture} alt={altname} />
     </div>
-  )
+  );
 }
 
 function App() {
   return (
     <div>
       {
-        foodILike.map(dish => (
-            <Food key={dish.id} name={dish.name} picture={dish.image} />
-          )
+        foodILike.map((dish) => (
+          <Food key={dish.id} name={dish.name} picture={dish.image} />
+        ),
         )
       }
     </div>
