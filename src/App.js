5b2dff669905388ea0bcb94d735a2be06e9d79e9
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-07-27 23:33:59 +0900

    lol

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+const foodILike = [
+  {
+    key: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    key: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    key: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    key: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    key: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+]
+
 function Food({ name, picture }) {
-  const altname = "Supposed to be " + {name} + " picture"
+  const altname = "Supposed to be a " + name + " picture"
   return (
     <div>
       <h2>I like {name}</h2>
@@ -8,40 +41,15 @@ function Food({ name, picture }) {
   )
 }
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aerisitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-]
-
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (
-      <Food name={dish.name} picture={dish.image} />
-      ))}
+      {
+        foodILike.map(dish => (
+            <Food key={dish.id} name={dish.name} picture={dish.image} />
+          )
+        )
+      }
     </div>
   );
 }
