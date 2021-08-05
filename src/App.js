d9ffa25cd0ddaabdf0760b0cc9c739f05b69f1ad
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-08-05 10:16:07 +0900

    lol

--- src/App.js
+++ src/App.js
@@ -1,77 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    key: 1,
-    name: 'Kimchi',
-    image:
-      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 1,
-  },
-  {
-    key: 2,
-    name: 'Samgyeopsal',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-    rating: 2,
-  },
-  {
-    key: 3,
-    name: 'Bibimbap',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-    rating: 3,
-  },
-  {
-    key: 4,
-    name: 'Doncasu',
-    image:
-      'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-    rating: 4,
-  },
-  {
-    key: 5,
-    name: 'Kimbap',
-    image:
-      'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-    rating: 5,
-  },
-];
-
-function Food({name, picture, rating}) {
-  const altname = name + ' picture';
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>Rating: {rating}</h4>
-      <img src={picture} alt={altname} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor')
+  }
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  render() {
+    console.log('render')
+    return (
+      <div>
+        <h1>Number: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div>
-      {
-        foodILike.map((dish) => (
-          <Food
-            key={dish.id}
-            name={dish.name}
-            picture={dish.image}
-            rating={dish.rating}
-          />
-        ),
-        )
-      }
-    </div>
-  );
-}
 
 export default App;
